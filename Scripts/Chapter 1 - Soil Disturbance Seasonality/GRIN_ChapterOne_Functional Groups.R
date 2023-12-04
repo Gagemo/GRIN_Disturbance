b7b4e729dcd2bdf83e7ef16f3d0ef07af094f13d
Gage LaPierre <gagemo@ufl.edu>
2023-12-04 16:57:33 -0500

    g

--- Scripts/Chapter 1 - Soil Disturbance Seasonality/GRIN_ChapterOne_Functional Groups.R
+++ Scripts/Chapter 1 - Soil Disturbance Seasonality/GRIN_ChapterOne_Functional Groups.R
@@ -1,11 +1,10 @@
+################################################################################
+################################################################################
 #########################   GRIN - Disturbance    ##############################
 #########################    Functional Groups    ##############################
 #########################  University of Florida  ##############################
 #########################     Gage LaPierre       ##############################
 #########################      2022 - 2023        ##############################
-################################################################################
-################################################################################
-################################################################################
 ################################################################################
 ################################################################################
 
@@ -30,8 +29,8 @@ library(agricolae)
 library(ggsignif)
 library(multcompView)
 
-##########################     Read in 2022 Data       #########################
-GRIN = read.csv("Data/GRIN_FUN-2022.csv")
+##########################     Read in 2020 - 2023 Data       ##################
+GRIN = read.csv("Data/GRIN_FUN-2021-2023.csv")
 GRIN$Coverage = as.numeric(GRIN$Coverage)
 GRIN$Plot = as.character(GRIN$Plot)
 
@@ -41,8 +40,15 @@ summary(GRIN)
 # Remove Seeding Treatment # 
 GRIN = filter(GRIN, Treatment != 'S')
 
+# Remove Seeding Treatment # 
+GRIN = filter(GRIN, Year != 3)
+
+# Replace NA Values with Zeros#
+GRIN$Coverage[is.na(GRIN$Coverage)] <- 0
+
 # Reclasifys coverage data (CV) from 1-10 scale to percent scale #
 GRIN <- mutate(GRIN, Coverage = case_when(
+  grepl(0, Coverage) ~ 0,
   grepl(1, Coverage) ~ 0.1,
   grepl(2, Coverage) ~ 0.5,
   grepl(3, Coverage) ~ 1.5,
@@ -55,34 +61,66 @@ GRIN <- mutate(GRIN, Coverage = case_when(
   grepl(10, Coverage) ~ 97.5
 ))
 
-# Replace NA Values with Zeros#
-GRIN$Coverage[is.na(GRIN$Coverage)] <- 0
+# Sums CV data #
+GRIN1 <- GRIN %>%
+  select(ID, Year, Treatment, Group, Species, Coverage) %>% 
+  group_by(ID, Year, Treatment, Group)  %>% 
+  dplyr::summarize(TotalCV = sum(Coverage))
+
+# Creates data sets by year #
+GRIN_21 = filter(GRIN1, Year == 1)
+GRIN_22 = filter(GRIN1, Year == 2)
 
 ################################################################################
 ##################### Functional Groups Analysis & Plot ########################
 ################################################################################
+########################### 2021 Data Set ######################################
+################################################################################
 
-GROUP <- GRIN %>% 
-  filter(Group != "Bare") %>%
-  select(ID, Treatment, Group, Species, Coverage) %>% 
-  group_by(ID, Treatment, Group)  %>% 
-  summarize(TotalCV = sum(Coverage))
+# Coverage by Functional Group per Plot Box Plot #
+Group_Box = 
+  ggplot(GRIN1, aes(x = Treatment, y = TotalCV, fill = Group)) +
+  geom_boxplot(alpha = 0.8, outlier.shape = NA ) +
+  facet_wrap(.~Year) +
+  scale_fill_manual(breaks=c('Bare', 'Forb', 'Grass', 'Sedge', 'Woody'),
+                    values = c("tan", "#660066", "#339966", "#E7B800", "brown"), 
+                    labels=c("Bare", "Forb", "Grass", "Sedge", 'Woody')) +
+  theme_classic() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank(),
+        plot.title = element_text(hjust = 0.5),
+        text=element_text(size=16),
+        axis.title.x = element_text(size=15, face="bold", colour = "black"),    
+        axis.title.y = element_text(size=15, face="bold", colour = "black"),   
+        axis.text.x=element_text(size=15, face = "bold", color = "black"),
+        axis.text.y=element_text(size=15, face = "bold", color = "black"),
+        strip.text.x = 
+          element_text(size = 15, colour = "black", face = "bold")) +
+  guides(fill = guide_legend(label.position = "bottom")) +
+  labs(x = "Treatment", y = "Total (%) Coverage")
+Group_Box
+
+###############################  Forbs 2021 ####################################
+Forb = filter(GRIN_21, Group == "Forb")
 
 # Test for Significance #
-Fun_anova = aov(TotalCV ~ Treatment + Group, data = GROUP)
+Fun_anova = aov(TotalCV ~ Treatment, data = Forb)
 summary(Fun_anova)
+
 tukey.one.way<-TukeyHSD(Fun_anova)
 tukey.one.way
-HSD.stat = HSD.test(Fun_anova,trt = c("Treatment", "Group")) #HSD Tukey 
-HSD.stat
 
-## Coverage by Functional Group per Plot Box Plot #
-Group_Box = 
-  ggplot(GROUP, aes(x = Treatment, y = TotalCV, fill = Group)) +
-  geom_boxplot(outlier.shape = NA ) +
-  geom_jitter(size = 2, alpha = 0.6, shape = 21, 
-              position = position_jitterdodge(jitter.width = 0.1))
-Group_Box
+###############################  Forbs 2022 ####################################
+Forb = filter(GRIN_22, Group == "Forb")
+
+# Test for Significance #
+Fun_anova = aov(TotalCV ~ Treatment, data = Forb)
+summary(Fun_anova)
+
+tukey.one.way<-TukeyHSD(Fun_anova)
+tukey.one.way
 
 ################################################################################
 ############################## Annual vs Perennial #############################
